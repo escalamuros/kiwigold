96619efaf1c180294bd1e1afa6f9f95135a218d9
escalamuros <escalamuros@gmail.com>
2014-09-15 02:36:35 -0300

    completado laboratorio, lab autoriza calcula mediante seleccion de frutos

--- lab_autorisa.php
+++ lab_autorisa.php
@@ -12,37 +12,46 @@ $c->desconexion();
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <script>
 $(document).ready(function(){
-
 	var minpeso=999999,maxpeso=0,minpres=999999,maxpres=0,mincol=999999,maxcol=0,minss=999999,maxss=0,minseca=999999,maxseca=0;
-	//al cambiar un cuadro, evalua rangos,calcula y muestra (laboratorio.php)
-/*	$('.cuadrito').change(function(e)
-	{	
-		var fo=this.id.substring(0,6);
-		var fu=this.id.substring(6);
-		rangos(fo,fu);
-		calcular(fu);
-		desviaciones();
-	});
-	function rangos(fo,fu)
+	$('#man_datos').hide();
+	function completarCampos(per)
 	{
-		var nu=$('#'+fo+fu).val(),box=$('#'+fo+fu);
-		if (fo=='l_peso')
+		for(var de=0;de<=47;de++)
 		{
-			if(nu<60 || nu>191){alert('Sus valores estan fuera de rango permitido. De todas formas, estos se guardaran para referencia');}
-		}
-		if (fo=='l_pre1' || fo=='l_pre2' )
-		{
-			if(nu<8 || nu>17){alert('Sus valores estan fuera de rango permitido. De todas formas, estos se guardaran para referencia');}
-		}
-		if (fo=='l_solu')
-		{
-			if(nu<3.7 || nu>12){alert('Sus valores estan fuera de rango permitido. De todas formas, estos se guardaran para referencia');}
-		}
-		if (fo=='l_col1' || fo=='l_col2')
-		{
-			if(nu<103 || nu>115){alert('Sus valores estan fuera de rango permitido. De todas formas, estos se guardaran para referencia');}
+			$('#l_peso'+(de+1)).val(per[de][1]);
+			$('#l_pre1'+(de+1)).val(per[de][2]);
+			$('#l_pre2'+(de+1)).val(per[de][3]);
+			$('#l_solu'+(de+1)).val(per[de][4]);
+			$('#l_col1'+(de+1)).val(per[de][5]);
+			$('#l_col2'+(de+1)).val(per[de][6]);
+			$('#l_pesi'+(de+1)).val(per[de][7]);
+			$('#l_pesf'+(de+1)).val(per[de][8]);
+			$('#l_obse'+(de+1)).val(per[de][9]);
+			if(per[de][10]==1){$('#l_chke'+(de+1)).prop('checked', true);}
 		}
 	}
+	$('.labs').bind('click',function(){
+		var oe=$(this).attr('id');
+		$('#existe_lab').val(oe);
+		$.ajax({
+				url:'recibeajax.php',
+				type:'POST',
+				data:{lab_seleccionado:oe},
+				dataType:"json",
+				success:function(te){ completarCampos(te);for(var de=1;de<=48;de++){calcular(de);}desviaciones(); 	}
+		});
+		
+		$('#man_datos').show();
+	});
+	//al cambiar un checkbox, calcula y muestra 
+	$('.contarlo').change(function(e)
+	{	
+		var num=$(this).attr('id').substring(6);
+		//if($(this).is(':checked')){}else{}
+		calcular(num);
+		desviaciones();
+	});
+	
 	function calcular(fu)
 	{
 		var p1=$('#l_pre1'+fu).val();
@@ -64,104 +73,90 @@ $(document).ready(function(){
 		}else{$('#p_colo'+fu).html('');}
 		if(p5!='' && p6!='')
 		{
-			var total=(100*(parseFloat(p6)/parseFloat(p5))).toFixed(1);
+			if(p5>0){
+			var total=(100*(parseFloat(p6)/parseFloat(p5))).toFixed(1);}
+			else{ var total=0;}
 			$('#m_seca'+fu).html(total);
 		}else{$('#m_seca'+fu).html('');}
 	}
 	function desviaciones()
 	{
-		var sumapeso=0,validospeso=0,sumdifpeso,prompeso,difpeso=0;
-		var sumapres=0,validospres=0,sumdifpres,prompres,difpres=0;
-		var sumasss=0,validosss=0,sumdifss,promss,difss=0;
-		var sumacol=0,validoscol=0,sumdifcol,promcol,difcol=0;
-		var sumaseca=0,validosseca=0,sumdifseca,promseca,difseca=0;
+		var sumapeso=0,sumdifpeso,prompeso,difpeso=0;
+		var sumapres=0,sumdifpres,prompres,difpres=0;
+		var sumasss=0,sumdifss,promss,difss=0;
+		var sumacol=0,sumdifcol,promcol,difcol=0;
+		var sumaseca=0,sumdifseca,promseca,difseca=0;
+		var tot_elem=0;
 		for(var de=1;de<=48;de++)
 		{
-			if($('#l_peso'+de).val()>0)
+			if($('#l_chke'+de).is(':checked'))
 			{
-				sumapeso=parseFloat($('#l_peso'+de).val())+sumapeso;
-				validospeso++;
-				if (minpeso>parseFloat($('#l_peso'+de).val())){minpeso=parseFloat($('#l_peso'+de).val());}
-				if (maxpeso<parseFloat($('#l_peso'+de).val())){maxpeso=parseFloat($('#l_peso'+de).val());}
-			}
-			if($('#p_pres'+de).html()>0)
-			{
-				sumapres=parseFloat($('#p_pres'+de).html())+sumapres;
-				validospres++;
-				if (minpres>parseFloat($('#p_pres'+de).html())){minpres=parseFloat($('#p_pres'+de).html());}
-				if (maxpres<parseFloat($('#p_pres'+de).html())){maxpres=parseFloat($('#p_pres'+de).html());}
-			}
-			if($('#p_colo'+de).html()>0)
-			{
-				sumacol=parseFloat($('#p_colo'+de).html())+sumacol;
-				validoscol++;
-				if (mincol>parseFloat($('#p_colo'+de).html())){mincol=parseFloat($('#p_colo'+de).html());}
-				if (maxcol<parseFloat($('#p_colo'+de).html())){maxcol=parseFloat($('#p_colo'+de).html());}
-			}
-			if($('#l_solu'+de).val()>0)
-			{
-				sumasss=parseFloat($('#l_solu'+de).val())+sumasss;
-				validosss++;
-				if (minss>parseFloat($('#l_solu'+de).val())){minss=parseFloat($('#l_solu'+de).val());}
-				if (maxss<parseFloat($('#l_solu'+de).val())){maxss=parseFloat($('#l_solu'+de).val());}
-			}
-			if($('#m_seca'+de).html()>0)
-			{
-				sumaseca=parseFloat($('#m_seca'+de).html())+sumaseca;
-				validosseca++;
-				if (minseca>parseFloat($('#m_seca'+de).html())){minseca=parseFloat($('#m_seca'+de).html());}
-				if (maxseca<parseFloat($('#m_seca'+de).html())){maxseca=parseFloat($('#m_seca'+de).html());}
+				 tot_elem++;
+				 sumapeso=parseFloat($('#l_peso'+de).val())+sumapeso;
+				 if (minpeso>parseFloat($('#l_peso'+de).val())){minpeso=parseFloat($('#l_peso'+de).val());}
+				 if (maxpeso<parseFloat($('#l_peso'+de).val())){maxpeso=parseFloat($('#l_peso'+de).val());}
+				 sumapres=parseFloat($('#p_pres'+de).html())+sumapres;
+				 if (minpres>parseFloat($('#p_pres'+de).html())){minpres=parseFloat($('#p_pres'+de).html());}
+				 if (maxpres<parseFloat($('#p_pres'+de).html())){maxpres=parseFloat($('#p_pres'+de).html());}
+				 sumacol=parseFloat($('#p_colo'+de).html())+sumacol;
+				 if (mincol>parseFloat($('#p_colo'+de).html())){mincol=parseFloat($('#p_colo'+de).html());}
+				 if (maxcol<parseFloat($('#p_colo'+de).html())){maxcol=parseFloat($('#p_colo'+de).html());}
+				 sumasss=parseFloat($('#l_solu'+de).val())+sumasss;
+				 if (minss>parseFloat($('#l_solu'+de).val())){minss=parseFloat($('#l_solu'+de).val());}
+				 if (maxss<parseFloat($('#l_solu'+de).val())){maxss=parseFloat($('#l_solu'+de).val());}
+				 sumaseca=parseFloat($('#m_seca'+de).html())+sumaseca;
+				 if (minseca>parseFloat($('#m_seca'+de).html())){minseca=parseFloat($('#m_seca'+de).html());}
+				 if (maxseca<parseFloat($('#m_seca'+de).html())){maxseca=parseFloat($('#m_seca'+de).html());}
+				 
 			}
 		}
-		prompeso=(sumapeso/validospeso).toFixed(1);
-		prompres=(sumapres/validospres).toFixed(1);
-		promcol=(sumacol/validoscol).toFixed(1);
-		promss=(sumasss/validosss).toFixed(1);
-		promseca=(sumaseca/validosseca).toFixed(1);
+		prompeso=(sumapeso/tot_elem).toFixed(1);
+		prompres=(sumapres/tot_elem).toFixed(1);
+		promcol=(sumacol/tot_elem).toFixed(1);
+		promss=(sumasss/tot_elem).toFixed(1);
+		promseca=(sumaseca/tot_elem).toFixed(1);
 		for (var ede=1;ede<=48;ede++)
-		{	
-			if($('#l_peso'+ede).val()>0){
-			sumdifpeso=Math.pow(parseFloat($('#l_peso'+ede).val())-prompeso,2);
-			difpeso=difpeso+sumdifpeso;}
-			if($('#p_pres'+ede).html()>0){
-			sumdifpres=Math.pow(parseFloat($('#p_pres'+ede).html())-prompres,2);
-			difpres=difpres+sumdifpres;}
-			if($('#p_colo'+ede).html()>0){
-			sumdifcol=Math.pow(parseFloat($('#p_colo'+ede).html())-promcol,2);
-			difcol=difcol+sumdifcol;}
-			if($('#l_solu'+ede).val()>0){
-			sumdifss=Math.pow(parseFloat($('#l_solu'+ede).val())-promss,2);
-			difss=difss+sumdifss;}
-			if($('#m_seca'+ede).html()>0){
-			sumdifseca=Math.pow(parseFloat($('#m_seca'+ede).html())-promseca,2);
-			difseca=difseca+sumdifseca;}
+		{
+			if($('#l_chke'+ede).is(':checked'))
+			{
+				sumdifpeso=Math.pow(parseFloat($('#l_peso'+ede).val())-prompeso,2);
+				difpeso=difpeso+sumdifpeso;
+				sumdifpres=Math.pow(parseFloat($('#p_pres'+ede).html())-prompres,2);
+				difpres=difpres+sumdifpres;
+				sumdifcol=Math.pow(parseFloat($('#p_colo'+ede).html())-promcol,2);
+				difcol=difcol+sumdifcol;
+				sumdifss=Math.pow(parseFloat($('#l_solu'+ede).val())-promss,2);
+				difss=difss+sumdifss;
+				sumdifseca=Math.pow(parseFloat($('#m_seca'+ede).html())-promseca,2);
+				difseca=difseca+sumdifseca;
+			}
 		}
-		var desv=Math.sqrt(difpeso/validospeso).toFixed(1);
-		var desvp=Math.sqrt(difpres/validospres).toFixed(1);
-		var desvc=Math.sqrt(difcol/validoscol).toFixed(1);
-		var desvs=Math.sqrt(difss/validosss).toFixed(1);
-		var desvm=Math.sqrt(difseca/validosseca).toFixed(1);
-		$('#resvalidos').html(validospeso);
+		var desv=Math.sqrt(difpeso/tot_elem).toFixed(1);
+		var desvp=Math.sqrt(difpres/tot_elem).toFixed(1);
+		var desvc=Math.sqrt(difcol/tot_elem).toFixed(1);
+		var desvs=Math.sqrt(difss/tot_elem).toFixed(1);
+		var desvm=Math.sqrt(difseca/tot_elem).toFixed(1);
+		$('#resvalidos').html(tot_elem);
 		$('#resprom').html(prompeso);
 		$('#resmin').html(minpeso);
 		$('#resmax').html(maxpeso);
 		$('#resdesv').html(desv);
-		$('#resvalidosp').html(validospres);
+		$('#resvalidosp').html(tot_elem);
 		$('#respromp').html(prompres);
 		$('#resminp').html(minpres);
 		$('#resmaxp').html(maxpres);
 		$('#resdesvp').html(desvp);
-		$('#resvalidosc').html(validoscol);
+		$('#resvalidosc').html(tot_elem);
 		$('#respromc').html(promcol);
 		$('#resminc').html(mincol);
 		$('#resmaxc').html(maxcol);
 		$('#resdesvc').html(desvc);
-		$('#resvalidoss').html(validosss);
+		$('#resvalidoss').html(tot_elem);
 		$('#resproms').html(promss);
 		$('#resmins').html(minss);
 		$('#resmaxs').html(maxss);
 		$('#resdesvs').html(desvs);
-		$('#resvalidosm').html(validosseca);
+		$('#resvalidosm').html(tot_elem);
 		$('#respromm').html(promseca);
 		$('#resminm').html(minseca);
 		$('#resmaxm').html(maxseca);
@@ -176,85 +171,12 @@ $(document).ready(function(){
 		$('#maxcol').html((parseFloat(promcol)+3.35*parseFloat(desvc)).toFixed(1));
 		$('#minseca').html((promseca-3.35*desvm).toFixed(1));
 		$('#maxseca').html((parseFloat(promseca)+3.35*parseFloat(desvm)).toFixed(1));
-		$('#deppeso').html(((prompeso*validospeso-minpeso-maxpeso)/(validospeso-2)).toFixed(1));
-		$('#deppre').html(((prompres*validospres-minpres-maxpres)/(validospres-2)).toFixed(1));
-		$('#depss').html(((promss*validosss-minss-maxss)/(validosss-2)).toFixed(1));
-		$('#depcol').html(((promcol*validoscol-mincol-maxcol)/(validoscol-2)).toFixed(1));
-		$('#depseca').html(((promseca*validosseca-minseca-maxseca)/(validosseca-2)).toFixed(1));
-	}*/
-	
-	//al cambiar la fecha de la analisis, genera nueva fecha, en f_analisis(laboratorio.php) 
-	/*$('#fmuestra').change(function(e)
-	{
-		$.ajax({
-			url:'recibeajax.php',
-			type:'POST',
-			data:{nuevafecha:$('#fmuestra').val(),fin:sessionStorage['ning']}
-		});
-	});*/
-	// sube los datos, con un session? (laboratorio.php)
-/*	$('#btn_subir').click(function(e)
-	{
-		$.ajax({
-			url:'recibeajax.php',
-			type:'POST',
-			data:{ subir_data:sessionStorage['ning']},
-			success:function(){alert ('Informacion subida con exito!')}
-		});
-	});*/
-
-/*
-	$('#fanalisis').change(function(e)
-	{ // AQUI REVISA SI EXISTEN ANALISIS
-		$.ajax({
-			url:'recibeajax.php',
-			type:'POST',
-			data:{um:$('#flab').val(),fecha:$('#fanalisis').val()},
-			success:function(e){ if(e!=''){traerDatos(e);}else{crearDatos();} }
-		});
-		$('#add_data').show();
-		$('#resultados').show();
-	});
-	function crearDatos()
-	{
-		for(var de=1;de<=48;de++)
-		{
-			$('#l_peso'+de).val('');
-			$('#l_pre1'+de).val('');
-			$('#l_pre2'+de).val('');
-			$('#l_solu'+de).val('');
-			$('#l_col1'+de).val('');
-			$('#l_col2'+de).val('');
-			$('#l_pesi'+de).val('');
-			$('#l_pesf'+de).val('');
-			$('#l_obse'+de).val('');
-			calcular(de);
-			desviaciones();
-		}
-		$.ajax({
-			url:'recibeajax.php',
-			type:'POST',
-			data:{lab:$('select#flab').val(),fecha:$('#fanalisis').val()},
-			success:function(e){ sessionStorage['ning']=e; }
-		});
+		$('#deppeso').html(((prompeso*tot_elem-minpeso-maxpeso)/(tot_elem-2)).toFixed(1));
+		$('#deppre').html(((prompres*tot_elem-minpres-maxpres)/(tot_elem-2)).toFixed(1));
+		$('#depss').html(((promss*tot_elem-minss-maxss)/(tot_elem-2)).toFixed(1));
+		$('#depcol').html(((promcol*tot_elem-mincol-maxcol)/(tot_elem-2)).toFixed(1));
+		$('#depseca').html(((promseca*tot_elem-minseca-maxseca)/(tot_elem-2)).toFixed(1));
 	}
-	function completarCampos(per)
-	{
-		for(var de=1;de<=48;de++)
-		{
-			$('#l_peso'+de).val(per[de][1]);
-			$('#l_pre1'+de).val(per[de][2]);
-			$('#l_pre2'+de).val(per[de][3]);
-			$('#l_solu'+de).val(per[de][4]);
-			$('#l_col1'+de).val(per[de][5]);
-			$('#l_col2'+de).val(per[de][6]);
-			$('#l_pesi'+de).val(per[de][7]);
-			$('#l_pesf'+de).val(per[de][8]);
-			$('#l_obse'+de).val(per[de][9]);
-			calcular(de);
-			desviaciones();
-		}	
-	}*/
 	
 });
 </script>
@@ -267,17 +189,47 @@ if(isset($_SESSION['id']))
 	{?>
 		<div id='lista_t_lab'>
 			<table>
-			<tr><td>Revisar</td><td>UM</td><td>Fecha de Lab</td><td>Fecha Muestreo</td><td>Estado</td></tr>
+			<tr><td>Lab Numero</td><td>UM</td><td>Campo</td><td>Productor</td><td>Fecha de Lab</td><td>Fecha Muestreo</td><td>Estado</td></tr>
 			<?php
 			foreach($ar as $i)
 			{
-				echo "<tr><td><div class='btn_color' id='".$i[0]."'>".$i[0]."</div></td><td>".$i[1]."</td><td>".$i[2]."</td><td>".$i[3]."</td><td>".$i[4]."</td></tr>";
+				echo "<tr><td><div class='btn_color labs' id='".$i[0]."'>".$i[0]."</div></td><td>".$i[1]."</td><td>".$i[2]."</td><td>".$i[3]."</td><td>".$i[4]."</td><td>".$i[5]."</td><td>";
+				switch($i[6])
+				{case 0: echo "Datos Incompletos";break;case 1: echo "Datos Completos";break;case 2: echo "Informe Enviado";break;}
+				echo "</td></tr>";
 			}
 			?>
 			</table>
 		</div>
-		<div id="resultados">
-			<table border="0">
+		<div id="man_datos">
+			<table>
+			<tr><td>Dato Valido</td><td >Nº</td><td >Peso(g)</td><td >Presion 1(lbs)</td><td >Presion 2(lbs)</td>
+				<td >Promedio Presion 1-2</td>
+				<td>SS (ºbrix)</td><td>Color 1(ºH)</td><td>Color 2(ºH)</td>
+				<td>Promedio Color 1-2</td>
+				<td>Peso Neto inicial(g)</td><td>Peso Neto final(g)</td>
+				<td>Mat Seca</td> 
+				<td>Observaciones</td></tr>
+			<?php
+					for($a=1;$a<=48;$a++){
+					echo '<tr><td><div id="nummer'.$a.'">'.$a.'</div></td>';
+					echo '<td><input type="checkbox" id="l_chke'.$a.'" class="contarlo"></td>';
+					echo '<td><input type="text" id="l_peso'.$a.'" size="3" class="cuadrito" /></td>';
+					echo '<td><input type="text" id="l_pre1'.$a.'" class="cuadrito" size="3"/></td>';
+					echo '<td><input type="text" id="l_pre2'.$a.'" class="cuadrito" size="3"/></td>';
+					echo '<td><div id="p_pres'.$a.'" class="es_1"></div></td>';
+					echo '<td><input type="text" id="l_solu'.$a.'" class="cuadrito" size="3"/></td>';
+					echo '<td><input type="text" id="l_col1'.$a.'" class="cuadrito" size="3"/></td>';
+					echo '<td><input type="text" id="l_col2'.$a.'" class="cuadrito" size="3"/></td>';
+					echo '<td><div id="p_colo'.$a.'" class="es_1"></div></td>';
+					echo '<td><input type="text" id="l_pesi'.$a.'" class="cuadrito" size="3"/></td>';
+					echo '<td><input type="text" id="l_pesf'.$a.'" class="cuadrito" size="3"/></td>';
+					echo '<td><div id="m_seca'.$a.'" class="es_1"></div></td>';
+					echo '<td><input type="text" id="l_obse'.$a.'" class="cuadrito" size="28"/></td></tr>';	
+					}	
+				?>
+			</table>
+			<table>
 			<tr><td></td><td>Peso(g)</td><td>Promedio Presion 1-2</td><td>SS (ºbrix)</td><td>Promedio Color 1-2</td><td>Mat Seca</td></tr>
 			<tr><td>Datos Válidos</td><td><div id="resvalidos" class="resul"></div></td><td><div id="resvalidosp" class="resul"></td><td><div id="resvalidoss" class="resul"></td><td><div id="resvalidosc" class="resul"></td><td><div id="resvalidosm" class="resul"></td></tr>
 			<tr><td>Promedio Aritmetico</td><td><div id="resprom" class="resul"></div></td><td><div id="respromp" class="resul"></td><td><div id="resproms" class="resul"></td><td><div id="respromc" class="resul"></td><td><div id="respromm" class="resul"></td></tr>
